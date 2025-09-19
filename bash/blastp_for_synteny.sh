01025a0433f40c8f5b3b245455b05d874b5aa1b4
milena-t <milena.trabert@t-online.de>
2025-09-19 17:57:22 +0200

    modify fasta headers

--- bash/blastp_for_synteny.sh
+++ bash/blastp_for_synteny.sh
@@ -10,11 +10,17 @@
 
 module load bioinfo-tools blast/2.15.0+
 
-A_obtectus_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/A_obtectus.faa
-B_siliquastri_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/B_siliquastri.faa
-C_chinensis_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/C_chinensis.faa
-C_maculatus_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/C_maculatus_superscaffolded.faa
-T_castaneum_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/T_castaneum.faa
+# sed 's/>B_siliquastri__B_siliquastri__B_siliquastri_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/B_siliquastri.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/B_siliquastri_original_header.faa
+# sed 's/A_obtectus__A_obtectus__/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/A_obtectus.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/A_obtectus_original_header.faa
+# sed 's/>C_chinensis__C_chinensis__C_chinensis_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_chinensis.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_chinensis_original_header.faa
+# sed 's/>Cmac_Lome_diverse_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_maculatus_superscaffolded.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/C_maculatus_superscaffolded_original_header.faa
+# sed 's/>T_castaneum__T_castaneum__T_castaneum_/>/g' /proj/naiss2023-6-65/Milena/chapter2/protein_data/T_castaneum.faa > /proj/naiss2023-6-65/Milena/chapter2/protein_data/T_castaneum_original_header.faa
+
+A_obtectus_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/A_obtectus_original_header.faa
+B_siliquastri_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/B_siliquastri_original_header.faa
+C_chinensis_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/C_chinensis_original_header.faa
+C_maculatus_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/C_maculatus_superscaffolded_original_header.faa
+T_castaneum_proteins=/proj/naiss2023-6-65/Milena/chapter2/protein_data/T_castaneum_original_header.faa
 
 ## make databases
 for SPECIES1 in $A_obtectus_proteins $B_siliquastri_proteins $C_chinensis_proteins $C_maculatus_proteins $T_castaneum_proteins
@@ -42,10 +48,11 @@ do
         OUT_1v2="${SPECIES1_name}_vs_${SPECIES2_name}.blast"
         OUT_2v1="${SPECIES2_name}_vs_${SPECIES1_name}.blast"
 
-        blastp -query $SPECIES1 -db $SPECIES2 -out $OUT_1v2 -num_threads 5 -max_target_seqs 5 -evalue 1e-10  -outfmt 8
+        # the documentation says outfmt6 but I think they mean 8
+        blastp -query $SPECIES1 -db $SPECIES2 -out $OUT_1v2 -num_threads 5 -max_target_seqs 5 -evalue 1e-10  -outfmt 6
         echo " =========> ${OUT_1v2} done!"
 
-        blastp -query $SPECIES2 -db $SPECIES1 -out $OUT_2v1 -num_threads 5 -max_target_seqs 5 -evalue 1e-10  -outfmt 8
+        blastp -query $SPECIES2 -db $SPECIES1 -out $OUT_2v1 -num_threads 5 -max_target_seqs 5 -evalue 1e-10  -outfmt 6
         echo " =========> ${OUT_2v1} done!"
 
     done
