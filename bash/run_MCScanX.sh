65b8fd4f96c756926af13ce7cf8e7e510a4812f7
milena-t <milena.trabert@t-online.de>
2025-09-22 11:15:18 +0200

    fix path

--- bash/run_MCScanX.sh
+++ bash/run_MCScanX.sh
@@ -3,11 +3,11 @@
 #SBATCH -p core
 #SBATCH -n 1
 #SBATCH -t 2:00:00
-#SBATCH -J blastp_for_synteny
-#SBATCH -o blastp_for_synteny.log
+#SBATCH -J run_MCScanX
+#SBATCH -o run_MCScanX.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
 module load bioinfo-tools 
 
-./proj/naiss2023-6-65/Milena/chapter2/MCScanX-1.0.0/MCScanX /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results
+/proj/naiss2023-6-65/Milena/chapter2/MCScanX-1.0.0/MCScanX /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results
