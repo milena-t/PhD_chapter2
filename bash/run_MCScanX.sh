177387c622969f02097e920a3cfdc58b4247b258
milena-t <milena.trabert@t-online.de>
2025-09-22 11:33:16 +0200

    update time

--- bash/run_MCScanX.sh
+++ bash/run_MCScanX.sh
@@ -2,7 +2,7 @@
 #SBATCH -A uppmax2025-2-148
 #SBATCH -p core
 #SBATCH -n 1
-#SBATCH -t 2:00:00
+#SBATCH -t 1:00:00
 #SBATCH -J run_MCScanX
 #SBATCH -o run_MCScanX.log
 #SBATCH --mail-type=ALL
@@ -10,4 +10,6 @@
 
 module load bioinfo-tools 
 
+
+## runs really fast for five coleopteran species
 /proj/naiss2023-6-65/Milena/chapter2/MCScanX-1.0.0/MCScanX /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results
