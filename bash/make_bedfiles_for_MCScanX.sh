af4ef295e3d1e239c1033f01dcca68752e15f8a9
milena-t <milena.trabert@t-online.de>
2025-09-22 14:18:33 +0200

    global paths

--- bash/make_bedfiles_for_MCScanX.sh
+++ bash/make_bedfiles_for_MCScanX.sh
@@ -10,13 +10,13 @@
 
 module load bioinfo-tools biopython/1.80-py3.10.8
 
-python3 ../src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/A_obtectus.gff ao
+python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/A_obtectus.gff ao
 echo "done A_obtectus.gff"
-python3 ../src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/B_siliquastri.gff bs
+python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/B_siliquastri.gff bs
 echo "done B_siliquastri.gff"
-python3 ../src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/C_chinensis.gff cc
+python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/C_chinensis.gff cc
 echo "done C_chinensis.gff"
-python3 ../src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/C_maculatus_superscaffolded.gff cm
+python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/C_maculatus_superscaffolded.gff cm
 echo "done C_maculatus_superscaffolded.gff"
-python3 ../src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/T_castaneum.gff tc
+python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/T_castaneum.gff tc
 echo "done T_castaneum.gff"
