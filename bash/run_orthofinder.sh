7d5693916a08e7afa700c3b4cb5ddbde20113a59
milena-t <milena.trabert@t-online.de>
2025-09-25 14:56:36 +0200

    rerun orthofinder

--- bash/run_orthofinder.sh
+++ bash/run_orthofinder.sh
@@ -11,7 +11,7 @@
 module load bioinfo-tools OrthoFinder/2.5.2
 
 # the output directory will be created by orthofinder and should NOT already exist!
-OUT_DIR=/proj/naiss2023-6-65/Milena/chapter2/orthofinder
+OUT_DIR=/proj/naiss2023-6-65/Milena/chapter2/orthofinder/orthofinder_Cmac_Kaufmann2023
 
 # orthofinder runs with protein sequences here! nucleotide sequences require the -d flag
 IN_FASTA_DIR=/proj/naiss2023-6-65/Milena/chapter2/protein_data/orthofinder_dir
