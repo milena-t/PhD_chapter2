bcf59157d6ab106ad2dcfdea180259b1052db115
milena-t <milena.trabert@t-online.de>
2025-11-13 10:22:04 +0100

    download seq loop

--- bash/Tcas_Y/download_reads.sh
+++ bash/Tcas_Y/download_reads.sh
@@ -3,18 +3,15 @@
 #SBATCH -n 1
 #SBATCH -p core
 #SBATCH -t 1:00:00
-#SBATCH -J SRR23732240_download_sequences
-#SBATCH -o SRR23732240_download_sequences.log
+#SBATCH -J SRR23732241_download_sequences
+#SBATCH -o SRR23732241_download_sequences.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
 # Bioinfo tools not needed on pelle
-module load bioinfo-tools EDirect
+# module load bioinfo-tools EDirect
 
-## SRR IDS
-curl -O https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc=SRR23732240
-# curl -O https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc=SRR23732241
-# curl -O https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc=SRR23732242
-# curl -O https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc=SRR23732243
-# curl -O https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc=SRR23732244
-# curl -O https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc=SRR23732245
+
+
+## use with SRR IDS from PhD_chapter2/bash/Tcas_Y/SRR_list.sh
+curl -O https://trace.ncbi.nlm.nih.gov/Traces/sra-reads-be/fastq?acc=$1
