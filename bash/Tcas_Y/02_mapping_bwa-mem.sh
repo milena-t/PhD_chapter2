390bcd3fdda36f941ff0cdd41059faaf829209c0
milena-t <milena.trabert@t-online.de>
2025-11-13 16:43:08 +0100

    mapping

---
+++ bash/Tcas_Y/02_mapping_bwa-mem.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#SBATCH -A uppmax2025-2-148
+#SBATCH -p core
+#SBATCH -n 16
+#SBATCH -t 2:00:00
+#SBATCH -J mapping_bwa
+#SBATCH -o mapping_bwa.log
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user milena.trabert@ebc.uu.se
+
+module load bioinfo-tools  python3  bwa/0.7.17 samtools/1.17 bwa-mem2/2.2.1-20211213-edc703f
+
+ref_dir=/proj/naiss2023-6-65/Milena/chapter2/Tribolium_poolseq
+
+TCAS_index=T_castaneum_index_for_bwa
+
+### index reference before mapping 
+bwa index -p $TCAS_index "${ref_dir}/T_castaneum_assembly.fna"
+
+# Directory with reads
+reads_dir="/proj/naiss2023-6-65/Milena/chapter2/Tribolium_poolseq/raw_data/trimmed_fastp"
+mapped_dir="/proj/naiss2023-6-65/Milena/chapter2/Tribolium_poolseq/mapped_data"
+
+for SRR_num in  SRR23732240 SRR23732241 SRR23732242 SRR23732243 SRR23732244 SRR23732245
+do
+    r1="${output_dir}/${SRR_num}_1_trimmed.fastq.gz"
+    r2="${output_dir}/${SRR_num}_2_trimmed.fastq.gz"
+
+    echo "======================>> Running bwa-mem on $sample ..."
+    bwa mem -t 20 -P $TCAS_index $r1 $r2 | samtools view -u | samtools sort -o "${mapped_dir}/${SRR_num}.bam"
+
+done
