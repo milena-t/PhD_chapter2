7ef5e300e06fe04ca7b8979b3f5916611bbdffe6
milena-t <milena.trabert@t-online.de>
2025-11-13 17:01:42 +0100

    mapping

--- bash/Tcas_Y/021_mapping_bwa_mem_ind.sh
+++ bash/Tcas_Y/021_mapping_bwa_mem_ind.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A uppmax2025-2-148
 #SBATCH -p core
-#SBATCH -n 1
+#SBATCH -n 20
 #SBATCH -t 20:00:00
 #SBATCH -J mapping_bwa
 #SBATCH -o mapping_bwa.log
@@ -22,7 +22,8 @@ bwa index -p $TCAS_index "${ref_dir}/T_castaneum_assembly.fna"
 reads_dir="/proj/naiss2023-6-65/Milena/chapter2/Tribolium_poolseq/raw_data/trimmed_fastp"
 mapped_dir="/proj/naiss2023-6-65/Milena/chapter2/Tribolium_poolseq/mapped_data"
 
-for SRR_num in  SRR23732240 SRR23732241 SRR23732242 SRR23732243 SRR23732244 SRR23732245
-do
-    sbatch -J "${SRR_num}_mapping" -o "${SRR_num}_mapping.out" /proj/naiss2023-6-65/Milena/chapter2/PhD_chapter2/bash/Tcas_Y/021_mapping_bwa_mem_ind.sh $SRR_num
-done
+SRR_num=$1
+r1="${output_dir}/${SRR_num}_1_trimmed.fastq.gz"
+r2="${output_dir}/${SRR_num}_2_trimmed.fastq.gz"
+echo "======================>> Running bwa-mem on $sample ..."
+bwa mem -t 20 -P $TCAS_index $r1 $r2 | samtools view -u | samtools sort -o "${mapped_dir}/${SRR_num}.bam"
