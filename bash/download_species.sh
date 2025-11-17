cff9955f1b386a6999175c1dfc2bc3b90cba4609
milena-t <milena.trabert@t-online.de>
2025-11-17 17:40:54 +0100

    download and unzip all

--- bash/download_species.sh
+++ bash/download_species.sh
@@ -9,13 +9,21 @@
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
 cd /proj/naiss2023-6-65/Milena/coleoptera_sequences/d_sublineata
-# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_protein.faa.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_protein.faa.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_protein.gff.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_cds_from_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_genomic.fna.gz
 gunzip GCF_026230105.1_icDioSubl1.1_protein.faa.gz
-# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_protein.gff.gz
 gunzip GCF_026230105.1_icDioSubl1.1_protein.gff.gz
+gunzip GCF_026230105.1_icDioSubl1.1_cds_from_genomic.fna.gz
+gunzip GCF_026230105.1_icDioSubl1.1_genomic.fna.gz
 
 cd /proj/naiss2023-6-65/Milena/coleoptera_sequences/d_carinulata
-# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/250/575/GCF_026250575.1_icDioCari1.1/GCF_026250575.1_icDioCari1.1_genomic.gff.gz 
-gunzip GCF_026250575.1_icDioCari1.1_genomic.gff.gz 
-# wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/250/575/GCF_026250575.1_icDioCari1.1/GCF_026250575.1_icDioCari1.1_protein.faa.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/250/575/GCF_026250575.1_icDioCari1.1/GCF_026250575.1_icDioCari1.1_protein.faa.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/250/575/GCF_026250575.1_icDioCari1.1/GCF_026250575.1_icDioCari1.1_genomic.gff.gz 
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/250/575/GCF_026250575.1_icDioCari1.1/GCF_026250575.1_icDioCari1.1_cds_from_genomic.fna.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/250/575/GCF_026250575.1_icDioCari1.1/GCF_026250575.1_icDioCari1.1_genomic.fna.gz
+gunzip GCF_026250575.1_icDioCari1.1_genomic.gff.gz
 gunzip GCF_026250575.1_icDioCari1.1_protein.faa.gz
+gunzip GCF_026250575.1_icDioCari1.1_cds_from_genomic.fna.gz
+gunzip GCF_026250575.1_icDioCari1.1_genomic.fna.gz
