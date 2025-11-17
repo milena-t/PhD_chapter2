1cf4cab6e8060f0a470e15f7d1699eeea2e06ebf
milena-t <milena.trabert@t-online.de>
2025-11-17 18:14:08 +0100

    fix filename

--- bash/download_species.sh
+++ bash/download_species.sh
@@ -10,11 +10,11 @@
 
 cd /proj/naiss2023-6-65/Milena/coleoptera_sequences/d_sublineata
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_protein.faa.gz
-wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_protein.gff.gz
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_genomic.gff.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_cds_from_genomic.fna.gz
 wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/026/230/105/GCF_026230105.1_icDioSubl1.1/GCF_026230105.1_icDioSubl1.1_genomic.fna.gz
 gunzip GCF_026230105.1_icDioSubl1.1_protein.faa.gz
-gunzip GCF_026230105.1_icDioSubl1.1_protein.gff.gz
+gunzip GCF_026230105.1_icDioSubl1.1_genomic.gff.gz
 gunzip GCF_026230105.1_icDioSubl1.1_cds_from_genomic.fna.gz
 gunzip GCF_026230105.1_icDioSubl1.1_genomic.fna.gz
 
