3a9a08cce6e1c661c1874c2a1c4924bfae2ef607
milena-t <milena.trabert@t-online.de>
2025-12-10 15:33:06 +0100

    fix memory

--- bash/whole_genome_alignments/whole_pipeline.sh
+++ bash/whole_genome_alignments/whole_pipeline.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -A uppmax2025-2-148
 #SBATCH -n 16
-#SBATCH --mem=48G
+#SBATCH --mem=120G
 #SBATCH -t 1-00:00:00
 #SBATCH -J mummer_alignment
 #SBATCH -o mummer_alignment.log
