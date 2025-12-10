03601a84801cfcecabcc153fceaab75faa423271
milena-t <milena.trabert@t-online.de>
2025-12-10 15:15:08 +0100

    typo

--- bash/whole_genome_alignments/whole_pipeline.sh
+++ bash/whole_genome_alignments/whole_pipeline.sh
@@ -38,7 +38,7 @@ echo " >>>>>>>>>> ALIGNMENT"
 echo "nucmer -maxmatch -p $OUT_PREFIX -t 16 $REFERENCE $QUERY"
 echo "... running nucmer alignment"
 
-$MUMMER_PATH/nucmer -maxmatch -p $OUT_PREFIX -t 16 $REFERENCE $QUERY
+$MUMMER_PATH/nucmer --maxmatch -p $OUT_PREFIX -t 16 $REFERENCE $QUERY
 NUCMER_OUTFILE="${OUT_PREFIX}.delta"
 echo "alignment done! -> ${NUCMER_OUTFILE}"
 echo 
