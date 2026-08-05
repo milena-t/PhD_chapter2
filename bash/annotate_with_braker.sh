71502b562e7acf421c73a84b7ecc146998c4fa47
milena-t <milena.trabert@t-online.de>
2026-08-05 16:22:11 +0200

    fix path

--- bash/annotate_with_braker.sh
+++ bash/annotate_with_braker.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-ORTHODB_ARTHROPODA=/proj/naiss2023-6-65/Milena/annotation_pipeline/annotation_protein_data/OrthoDB_Arthropoda_v11.fa
+ORTHODB_ARTHROPODA=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/annotation_pipeline/annotation_protein_data/OrthoDB_Arthropoda_v11.fa
 ASS_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/assemblies
 
 sbatch --job-name="A_obtectus_annotation" --output="A_obtectus_annotation.out" -t 5-00:00:00 \
-/proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh \
+/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/bash/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh \
 A_obtectus ${ASS_DIR}/A_obtectus.masked.fna \
 $ORTHODB_ARTHROPODA SRR24951591 SRR7881623 SRR7881624 SRR7881590 SRR7881628 SRR7881629 SRR7881630 SRR7881599 SRR7881600 SRR7881604 SRR7881609 SRR7881639 SRR7881638 SRR7881643 SRR7881642 SRR7881633 SRR7881631
 
@@ -14,6 +14,6 @@ $ORTHODB_ARTHROPODA SRR24951591 SRR7881623 SRR7881624 SRR7881590 SRR7881628 SRR7
 # SRR7881623 SRR7881624 SRR7881590 SRR7881628 SRR7881629 SRR7881630 SRR7881599 SRR7881600 SRR7881604 SRR7881609 SRR7881639 SRR7881638 SRR7881643 SRR7881642 SRR7881633 SRR7881631
 
 sbatch --job-name="C_chinensis_annotation" --output="C_chinensis_annotation.out" -t 5-00:00:00 \
-/proj/naiss2023-6-65/Milena/annotation_pipeline/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh \
+/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/bash/braker3_singularity_with_RNAseq_in_SNIC_TMP.sh \
 C_chinensis ${ASS_DIR}/C_chinensis.masked.fna \
 $ORTHODB_ARTHROPODA SRR6167202 SRR6167201 SRR6167204 SRR6167203 SRR6167200 SRR6167199 SRR16474572 SRR16474571 SRR19159849 SRR19159850 SRR19159851 SRR19159846 SRR19159847 SRR19159848 SRR18148874 SRR18148875 SRR18148876 SRR18148877 SRR18148878 SRR18148879 SRR18148880 SRR18148881 SRR18148882
