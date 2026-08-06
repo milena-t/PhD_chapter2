b001b00f6d7ce57a38fe098b784315770db40299
milena-t <milena.trabert@t-online.de>
2026-08-06 16:36:22 +0200

    try wga

--- bash/whole_genome_alignments/run_alignment.sh
+++ bash/whole_genome_alignments/run_alignment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
 
-ASS_DIR=/proj/naiss2023-6-65/Milena/chapter2/assemblies
-SCRIPT_DIR=/proj/naiss2023-6-65/Milena/chapter2/PhD_chapter2/bash/whole_genome_alignments
+ASS_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/assemblies
+SCRIPT_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/bash/whole_genome_alignments
 
-cd /proj/naiss2023-6-65/Milena/chapter2/pairwise_wga
+cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/pairwise_wga
 
 sbatch --job-name="Cmac_Aobt_WGA" --output="Cmac_Aobt_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/C_maculatus_superscaffolded_assembly.fna" "${ASS_DIR}/A_obtectus_assembly.fna"
 sbatch --job-name="Cmac_Bsil_WGA" --output="Cmac_Bsil_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/C_maculatus_superscaffolded_assembly.fna" "${ASS_DIR}/B_siliquastri_assembly.fna"
 sbatch --job-name="Cmac_Dcar_WGA" --output="Cmac_Dcar_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/C_maculatus_superscaffolded_assembly.fna" "${ASS_DIR}/D_carinulata_assembly.fna"
-sbatch --job-name="Diorhabda_WGA" --output="Diorhabda_WGA.out" $SCRIPT_DIR/whole_pipeline_rackham.sh "${ASS_DIR}/D_carinulata_assembly.fna" "${ASS_DIR}/D_sublineata_assembly.fna"
-sbatch --job-name="Dcar_Bsil_WGA" --output="Dcar_Bsil_WGA.out" $SCRIPT_DIR/whole_pipeline_rackham.sh "${ASS_DIR}/D_carinulata_assembly.fna" "${ASS_DIR}/B_siliquastri_assembly.fna"
+sbatch --job-name="Diorhabda_WGA" --output="Diorhabda_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/D_carinulata_assembly.fna" "${ASS_DIR}/D_sublineata_assembly.fna"
+sbatch --job-name="Dcar_Bsil_WGA" --output="Dcar_Bsil_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/D_carinulata_assembly.fna" "${ASS_DIR}/B_siliquastri_assembly.fna"
