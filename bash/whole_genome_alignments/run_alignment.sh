537bf2ba2d62a1932657298380c112024fdf298c
milena-t <milena.trabert@t-online.de>
2026-08-07 17:07:17 +0200

    test loop wga

--- bash/whole_genome_alignments/run_alignment.sh
+++ bash/whole_genome_alignments/run_alignment.sh
@@ -7,38 +7,66 @@ cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/pairwise_wga/sys
 
 A_obtectus=A_obtectus.masked.fna
 B_siliquastri=B_siliquastri.masked.fna
-B_varius=B_varius_assembly.fna
+B_varius=B_varius.masked.fna
 C_maculatus=C_maculatus.masked.fna
 C_chinensis=C_chinensis.masked.fna
-D_carinulata=D_carinulata_assembly.fna
-D_sublineata=D_sublineata_assembly.fna
+D_carinulata=D_carinulata.masked.fna
+D_sublineata=D_sublineata.masked.fna
 
-sbatch --job-name="A_obtectus_B_siliquastri_WGA" --output="A_obtectus_B_siliquastri_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${B_siliquastri}"
-sbatch --job-name="A_obtectus_B_varius_WGA" --output="A_obtectus_B_varius_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${B_varius}"
-sbatch --job-name="A_obtectus_C_maculatus_WGA" --output="A_obtectus_C_maculatus_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${C_maculatus}"
-sbatch --job-name="A_obtectus_C_chinensis_WGA" --output="A_obtectus_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${C_chinensis}"
-sbatch --job-name="A_obtectus_D_carinulata_WGA" --output="A_obtectus_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${D_carinulata}"
-sbatch --job-name="A_obtectus_D_sublineata_WGA" --output="A_obtectus_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${D_sublineata}"
 
-sbatch --job-name="B_siliquastri_B_varius_WGA" --output="B_siliquastri_B_varius_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${B_varius}"
-sbatch --job-name="B_siliquastri_C_maculatus_WGA" --output="B_siliquastri_C_maculatus_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${C_maculatus}"
-sbatch --job-name="B_siliquastri_C_chinensis_WGA" --output="B_siliquastri_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${C_chinensis}"
-sbatch --job-name="B_siliquastri_D_carinulata_WGA" --output="B_siliquastri_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${D_carinulata}"
-sbatch --job-name="B_siliquastri_D_sublineata_WGA" --output="B_siliquastri_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${D_sublineata}"
+for SPECIES1 in $A_obtectus $B_siliquastri $B_varius $C_maculatus $C_chinensis $D_carinulata $D_sublineata
+do  
 
-sbatch --job-name="B_varius_C_maculatus_WGA" --output="B_varius_C_maculatus_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${C_maculatus}"
-sbatch --job-name="B_varius_C_chinensis_WGA" --output="B_varius_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${C_chinensis}"
-sbatch --job-name="B_varius_D_carinulata_WGA" --output="B_varius_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${D_carinulata}"
-sbatch --job-name="B_varius_D_sublineata_WGA" --output="B_varius_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${D_sublineata}"
+    SPECIES1_name="${SPECIES1##*/}"
+    SPECIES1_name="${SPECIES1_name%.*}"
 
-sbatch --job-name="C_maculatus_C_chinensis_WGA" --output="C_maculatus_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_maculatus}" "${ASS_DIR}/${C_chinensis}"
-sbatch --job-name="C_maculatus_D_carinulata_WGA" --output="C_maculatus_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_maculatus}" "${ASS_DIR}/${D_carinulata}"
-sbatch --job-name="C_maculatus_D_sublineata_WGA" --output="C_maculatus_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_maculatus}" "${ASS_DIR}/${D_sublineata}"
+    for SPECIES2 in $A_obtectus $B_siliquastri $B_varius $C_maculatus $C_chinensis $D_carinulata $D_sublineata
+    do
 
-sbatch --job-name="C_chinensis_D_carinulata_WGA" --output="C_chinensis_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_chinensis}" "${ASS_DIR}/${D_carinulata}"
-sbatch --job-name="C_chinensis_D_sublineata_WGA" --output="C_chinensis_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_chinensis}" "${ASS_DIR}/${D_sublineata}"
+        SPECIES2_name="${SPECIES2##*/}"
+        SPECIES2_name="${SPECIES2_name%.*}"
 
-sbatch --job-name="D_carinulata_D_sublineata_WGA" --output="D_carinulata_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${D_carinulata}" "${ASS_DIR}/${D_sublineata}"
+        if [[ "${SPECIES1_name}" == "${SPECIES2_name}" ]]
+        then
+            continue
+        fi
+
+        echo "species1 : ${SPECIES1_name}"
+        echo "species2 : ${SPECIES2_name}"
+
+        echo ""
+
+    done
+done
+
+
+
+# sbatch --job-name="A_obtectus_B_siliquastri_WGA" --output="A_obtectus_B_siliquastri_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${B_siliquastri}"
+# sbatch --job-name="A_obtectus_B_varius_WGA" --output="A_obtectus_B_varius_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${B_varius}"
+# sbatch --job-name="A_obtectus_C_maculatus_WGA" --output="A_obtectus_C_maculatus_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${C_maculatus}"
+# sbatch --job-name="A_obtectus_C_chinensis_WGA" --output="A_obtectus_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${C_chinensis}"
+# sbatch --job-name="A_obtectus_D_carinulata_WGA" --output="A_obtectus_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${D_carinulata}"
+# sbatch --job-name="A_obtectus_D_sublineata_WGA" --output="A_obtectus_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${A_obtectus}" "${ASS_DIR}/${D_sublineata}"
+# 
+# sbatch --job-name="B_siliquastri_B_varius_WGA" --output="B_siliquastri_B_varius_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${B_varius}"
+# sbatch --job-name="B_siliquastri_C_maculatus_WGA" --output="B_siliquastri_C_maculatus_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${C_maculatus}"
+# sbatch --job-name="B_siliquastri_C_chinensis_WGA" --output="B_siliquastri_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${C_chinensis}"
+# sbatch --job-name="B_siliquastri_D_carinulata_WGA" --output="B_siliquastri_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${D_carinulata}"
+# sbatch --job-name="B_siliquastri_D_sublineata_WGA" --output="B_siliquastri_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_siliquastri}" "${ASS_DIR}/${D_sublineata}"
+# 
+# sbatch --job-name="B_varius_C_maculatus_WGA" --output="B_varius_C_maculatus_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${C_maculatus}"
+# sbatch --job-name="B_varius_C_chinensis_WGA" --output="B_varius_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${C_chinensis}"
+# sbatch --job-name="B_varius_D_carinulata_WGA" --output="B_varius_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${D_carinulata}"
+# sbatch --job-name="B_varius_D_sublineata_WGA" --output="B_varius_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${B_varius}" "${ASS_DIR}/${D_sublineata}"
+# 
+# sbatch --job-name="C_maculatus_C_chinensis_WGA" --output="C_maculatus_C_chinensis_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_maculatus}" "${ASS_DIR}/${C_chinensis}"
+# sbatch --job-name="C_maculatus_D_carinulata_WGA" --output="C_maculatus_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_maculatus}" "${ASS_DIR}/${D_carinulata}"
+# sbatch --job-name="C_maculatus_D_sublineata_WGA" --output="C_maculatus_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_maculatus}" "${ASS_DIR}/${D_sublineata}"
+# 
+# sbatch --job-name="C_chinensis_D_carinulata_WGA" --output="C_chinensis_D_carinulata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_chinensis}" "${ASS_DIR}/${D_carinulata}"
+# sbatch --job-name="C_chinensis_D_sublineata_WGA" --output="C_chinensis_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${C_chinensis}" "${ASS_DIR}/${D_sublineata}"
+# 
+# sbatch --job-name="D_carinulata_D_sublineata_WGA" --output="D_carinulata_D_sublineata_WGA.out" $SCRIPT_DIR/whole_pipeline.sh "${ASS_DIR}/${D_carinulata}" "${ASS_DIR}/${D_sublineata}"
 
 
 
---
+++ src/pairwise_wga_heatmap.py
@@ -0,0 +1,4 @@
+"""
+Plot a heatmap for all pairwise genome alignments. I will be inspired by this: https://www.nature.com/articles/s41586-024-07473-2/figures/1
+and they do: "percentage of reference, as shown on the x axis, covered by the query, as shown on the y axis"
+"""
