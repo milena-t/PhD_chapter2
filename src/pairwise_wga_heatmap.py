f8cfd305571583e33ae70c9232a47a06ff3f9eaa
milena-t <milena.trabert@t-online.de>
2026-08-10 14:26:19 +0200

    wga heatmap

--- src/pairwise_wga_heatmap.py
+++ src/pairwise_wga_heatmap.py
@@ -5,6 +5,9 @@ and they do: "percentage of reference, as shown on the x axis, covered by the qu
 
 import sex_chromosomes
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 
 def fasta_indices(username="milena"):
     """
@@ -35,69 +38,70 @@ def get_aln_coord_files(username="milena"):
     out_dict = {
         "A_obtectus" : {
             "B_siliquastri" : f"{dirname}ref_A_obtectus.masked_query_B_siliquastri.masked.delta_filtered_coords",
-            "B_varius" : f"{dirname}",
-            "C_chinensis" : f"{dirname}",
-            "C_maculatus" : f"{dirname}",
-            "D_carinulata" : f"{dirname}",
-            "D_sublineata" : f"{dirname}",
+            "B_varius" : f"{dirname}ref_A_obtectus.masked_query_B_varius.masked.delta_filtered_coords",
+            "C_chinensis" : f"{dirname}ref_A_obtectus.masked_query_C_chinensis.masked.delta_filtered_coords",
+            "C_maculatus" : f"{dirname}ref_A_obtectus.masked_query_C_maculatus.masked.delta_filtered_coords",
+            "D_carinulata" : f"{dirname}ref_A_obtectus.masked_query_D_carinulata.masked.delta_filtered_coords",
+            "D_sublineata" : f"{dirname}ref_A_obtectus.masked_query_D_sublineata.masked.delta_filtered_coords",
         },
         "B_siliquastri" : {
-            "A_obtectus" : f"{dirname}",
-            "B_varius" : f"{dirname}",
-            "C_chinensis" : f"{dirname}",
-            "C_maculatus" : f"{dirname}",
-            "D_carinulata" : f"{dirname}",
-            "D_sublineata" : f"{dirname}",
+            "A_obtectus" : f"{dirname}ref_B_siliquastri.masked_query_A_obtectus.masked.delta_filtered_coords",
+            "B_varius" : f"{dirname}ref_B_siliquastri.masked_query_B_varius.masked.delta_filtered_coords",
+            "C_chinensis" : f"{dirname}ref_B_siliquastri.masked_query_C_chinensis.masked.delta_filtered_coords",
+            "C_maculatus" : f"{dirname}ref_B_siliquastri.masked_query_C_maculatus.masked.delta_filtered_coords",
+            "D_carinulata" : f"{dirname}ref_B_siliquastri.masked_query_D_carinulata.masked.delta_filtered_coords",
+            "D_sublineata" : f"{dirname}ref_B_siliquastri.masked_query_D_sublineata.masked.delta_filtered_coords",
         },
         "B_varius" : {
-            "A_obtectus" : f"{dirname}",
-            "B_siliquastri" : f"{dirname}",
-            "C_chinensis" : f"{dirname}",
-            "C_maculatus" : f"{dirname}",
-            "D_carinulata" : f"{dirname}",
-            "D_sublineata" : f"{dirname}",
+            "A_obtectus" : f"{dirname}ref_B_varius.masked_query_A_obtectus.masked.delta_filtered_coords",
+            "B_siliquastri" : f"{dirname}ref_B_varius.masked_query_B_siliquastri.masked.delta_filtered_coords",
+            "C_chinensis" : f"{dirname}ref_B_varius.masked_query_C_chinensis.masked.delta_filtered_coords",
+            "C_maculatus" : f"{dirname}ref_B_varius.masked_query_C_maculatus.masked.delta_filtered_coords",
+            "D_carinulata" : f"{dirname}ref_B_varius.masked_query_D_carinulata.masked.delta_filtered_coords",
+            "D_sublineata" : f"{dirname}ref_B_varius.masked_query_D_sublineata.masked.delta_filtered_coords",
 
         },
         "C_chinensis" : {
-            "A_obtectus" : f"{dirname}",
-            "B_siliquastri" : f"{dirname}",
-            "B_varius" : f"{dirname}",
-            "C_maculatus" : f"{dirname}",
-            "D_carinulata" : f"{dirname}",
-            "D_sublineata" : f"{dirname}",
+            "A_obtectus" : f"{dirname}ref_C_chinensis.masked_query_A_obtectus.masked.delta_filtered_coords",
+            "B_siliquastri" : f"{dirname}ref_C_chinensis.masked_query_B_siliquastri.masked.delta_filtered_coords",
+            "B_varius" : f"{dirname}ref_C_chinensis.masked_query_B_varius.masked.delta_filtered_coords",
+            "C_maculatus" : f"{dirname}ref_C_chinensis.masked_query_C_maculatus.masked.delta_filtered_coords",
+            "D_carinulata" : f"{dirname}ref_C_chinensis.masked_query_D_carinulata.masked.delta_filtered_coords",
+            "D_sublineata" : f"{dirname}ref_C_chinensis.masked_query_D_sublineata.masked.delta_filtered_coords",
         },
         "C_maculatus" : {
-            "A_obtectus" : f"{dirname}",
-            "B_siliquastri" : f"{dirname}",
-            "B_varius" : f"{dirname}",
-            "C_chinensis" : f"{dirname}",
-            "D_carinulata" : f"{dirname}",
-            "D_sublineata" : f"{dirname}",
+            "A_obtectus" : f"{dirname}ref_C_maculatus.masked_query_A_obtectus.masked.delta_filtered_coords",
+            "B_siliquastri" : f"{dirname}ref_C_maculatus.masked_query_B_siliquastri.masked.delta_filtered_coords",
+            "B_varius" : f"{dirname}ref_C_maculatus.masked_query_B_varius.masked.delta_filtered_coords",
+            "C_chinensis" : f"{dirname}ref_C_maculatus.masked_query_C_chinensis.masked.delta_filtered_coords",
+            "D_carinulata" : f"{dirname}ref_C_maculatus.masked_query_D_carinulata.masked.delta_filtered_coords",
+            "D_sublineata" : f"{dirname}ref_C_maculatus.masked_query_D_sublineata.masked.delta_filtered_coords",
         },
         "D_carinulata" : {
-            "A_obtectus" : f"{dirname}",
-            "B_siliquastri" : f"{dirname}",
-            "B_varius" : f"{dirname}",
-            "C_chinensis" : f"{dirname}",
-            "C_maculatus" : f"{dirname}",
-            "D_sublineata" : f"{dirname}",
+            "A_obtectus" : f"{dirname}ref_D_carinulata.masked_query_A_obtectus.masked.delta_filtered_coords",
+            "B_siliquastri" : f"{dirname}ref_D_carinulata.masked_query_B_siliquastri.masked.delta_filtered_coords",
+            "B_varius" : f"{dirname}ref_D_carinulata.masked_query_B_varius.masked.delta_filtered_coords",
+            "C_chinensis" : f"{dirname}ref_D_carinulata.masked_query_C_chinensis.masked.delta_filtered_coords",
+            "C_maculatus" : f"{dirname}ref_D_carinulata.masked_query_C_maculatus.masked.delta_filtered_coords",
+            "D_sublineata" : f"{dirname}ref_D_carinulata.masked_query_D_sublineata.masked.delta_filtered_coords",
         },
         "D_sublineata" : {
-            "A_obtectus" : f"{dirname}",
-            "B_siliquastri" : f"{dirname}",
-            "B_varius" : f"{dirname}",
-            "C_chinensis" : f"{dirname}",
-            "C_maculatus" : f"{dirname}",
-            "D_carinulata" : f"{dirname}",
+            "A_obtectus" : f"{dirname}ref_D_sublineata.masked_query_A_obtectus.masked.delta_filtered_coords",
+            "B_siliquastri" : f"{dirname}ref_D_sublineata.masked_query_B_siliquastri.masked.delta_filtered_coords",
+            "B_varius" : f"{dirname}ref_D_sublineata.masked_query_B_varius.masked.delta_filtered_coords",
+            "C_chinensis" : f"{dirname}ref_D_sublineata.masked_query_C_chinensis.masked.delta_filtered_coords",
+            "C_maculatus" : f"{dirname}ref_D_sublineata.masked_query_C_maculatus.masked.delta_filtered_coords",
+            "D_carinulata" : f"{dirname}ref_D_sublineata.masked_query_D_carinulata.masked.delta_filtered_coords",
         }
     }
     return out_dict
 
-def count_ref_aln_coordinates(aln_filtered_coords_path, ref_faidx_dict):
+def count_ref_aln_coordinates(aln_filtered_coords_path, ref_faidx_dict, contig_list = [], verbose=False):
     """
     get the percentage of bases of each reference contig that are covered by the query. 
     full contig length is determined from the faidx file
     The first two column in the alignment coordinates are start and end of the alignment in the reference contig
+    if given a contig list, the percentage is the coverage across all contigs
     """
     out_cov_bases = {}
     with open(aln_filtered_coords_path, "r") as aln_filtered_coords:
@@ -108,16 +112,102 @@ def count_ref_aln_coordinates(aln_filtered_coords_path, ref_faidx_dict):
             else:
                 out_cov_bases[refcontig] = int(reflen)
 
-    out_perc_bases = {contig : num_bases / ref_faidx_dict[contig] for contig,num_bases in out_cov_bases.items()}
-    return out_perc_bases
+    if contig_list == []:
+        out_perc_bases = {contig : num_bases / ref_faidx_dict[contig] for contig,num_bases in out_cov_bases.items()}
+        return out_perc_bases
+    else:
+        all_len = 0
+        cov_len = 0
+        miss_cont = []
+        for contig in contig_list:
+            if contig in out_cov_bases:
+                cov_len += out_cov_bases[contig]
+                all_len += ref_faidx_dict[contig]
+            else:
+                miss_cont.append(contig)
+        if len(miss_cont) > 0 and verbose:
+            uncov_len = sum([ref_faidx_dict[contig] for contig in miss_cont])
+            print(f"\t{len(miss_cont)} contigs ({uncov_len} bp) out of {len(contig_list)} from input list not in alignment: {aln_filtered_coords_path} \n\t{miss_cont}")
+        if all_len > 0:
+            return cov_len / all_len
+        else:
+            return 0
+
+
+def make_array_for_heatmap(aln_coord_files, sex_chr_dict, chr = "X",verbose=False):
+    """
+    make np.array to plot heatmap
+    """
+    species_list = list(aln_coord_files.keys())
+    species_count =len(species_list)
+    species_index = {species : i for i,species in enumerate(species_list)}
+    perc_overlap = np.empty((species_count,species_count))
+    perc_overlap[:] = np.nan
+
+    for ref_species,query_dict in aln_coord_files.items():
+        if verbose:
+            print(f"==================== {ref_species} ====================")
+        index_1 = species_index[ref_species]
+        ref_X = sex_chr_dict[ref_species][chr]
+        for query_species,aln_coords_path in query_dict.items():
+            index_2 = species_index[query_species]
+            quot_bases = count_ref_aln_coordinates(aln_filtered_coords_path=aln_coords_path, ref_faidx_dict=faidx_dicts[ref_species], contig_list=ref_X)
+            perc_bases = quot_bases*100
+            perc_overlap[index_1, index_2] = perc_bases
+            if verbose:
+                print(f"{query_species} : {perc_bases:.4f} %")
     
+    return perc_overlap,species_list
+
+
+
+def plot_heatmap(counts_array, species_list, filename = "BRH_orthologs_heatmap.png", title = f"pairwise orthologs counts"):
+    """
+    plot the heatmap created in make_array_for_heatmap()
+    """
+
+    fs = 40
+    aspect_ratio = 18 / 18
+    height_pixels = 2000  # Height in pixels
+    width_pixels = int(height_pixels * aspect_ratio)  # Width in pixels
+    fig = plt.figure(figsize=(width_pixels / 100, height_pixels / 100), dpi=100)
+    ax = fig.add_subplot(111)
+
+    # cmap=mpl.colormaps["rainbow"]
+    cmap = LinearSegmentedColormap.from_list("red_to_orange",["#b82946", "#F2933A"])
+    # cbarlabel="number of pairwise 1-to-1 ortholgs"
+    im = ax.imshow(counts_array, cmap=cmap)
+
+    # create text annotations
+    for i in range(len(species_list)):
+        for j in range(len(species_list)):
+            try:
+                count = counts_array[i, j]
+                text = ax.text(j, i, f"{count:.1f}%",ha="center", va="center", color="w", fontsize = fs*0.75)
+            except:
+                continue
+
+    species_list = [species.replace("_", ". ") for species in species_list]
+    # Show all ticks and label them with the respective list entries
+    ax.set_xticks(range(len(species_list)), labels=species_list,rotation=45, ha="right", rotation_mode="anchor", fontsize = fs)
+    ax.set_yticks(range(len(species_list)), labels=species_list, fontsize = fs)
+    plt.title(label=title, fontsize = fs*1.3)
+    
+    plt.tight_layout()
+    # plt.show()
+    plt.savefig(filename, dpi = 300, transparent = False)
+    print(f"figure saved here: {filename}")
+
 
 if __name__ == "__main__":
-    username = "milena"
+    username = "miltr339"
     sex_chr_dict = sex_chromosomes.get_contig_names()
     faidx_dicts = fasta_indices(username=username)
     aln_coord_files = get_aln_coord_files(username=username)
-    test_perc_bases = count_ref_aln_coordinates(aln_filtered_coords_path=aln_coord_files["A_obtectus"]["B_siliquastri"], ref_faidx_dict=faidx_dicts["A_obtectus"])
-    for contig,perc in test_perc_bases.items():
-        print(f"{contig} : {perc:.4f}")
+    outdir_wga = f"/Users/{username}/work/PhD_code/PhD_chapter2/data/pairwise_wga"
+
+    for chr in ["X","Y"]:
+        perc_X_overlap,species_list = make_array_for_heatmap(aln_coord_files=aln_coord_files, sex_chr_dict=sex_chr_dict, chr=chr,verbose=False)
+        print(perc_X_overlap)
+        plot_heatmap(counts_array=perc_X_overlap, species_list=species_list, filename = f"{outdir_wga}/{chr}_chr_alignment_coverage_heatmap.png", title = f"{chr}-Chromosome aln. coverage")
 
