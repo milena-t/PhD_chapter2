05ea2308bc7c7b86b60e67324c32ec6eb5ae90bd
milena-t <milena.trabert@t-online.de>
2026-08-12 12:39:56 +0200

    partition

--- run_braker4_snakemake/run_braker4_snakemake.sh
+++ run_braker4_snakemake/run_braker4_snakemake.sh
@@ -6,7 +6,7 @@ cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/assemblies
 snakemake \
     --snakefile /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/software_install/BRAKER4/Snakefile \
     --executor slurm \
-    --default-resources slurm_partition=core mem_mb=120000 \
+    --default-resources slurm_partition=pelle mem_mb=120000 \
     --cores 20 \
     --jobs 50 \
     --use-singularity \
