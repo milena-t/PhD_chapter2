c749f3526762eca5e67de3c4dba06ec9ea9825d7
milena-t <milena.trabert@t-online.de>
2026-08-12 11:56:12 +0200

    try snakemake

--- run_braker4_snakemake/run_braker4_snakemake.sh
+++ run_braker4_snakemake/run_braker4_snakemake.sh
@@ -1,3 +1,16 @@
 #!/bin/bash -l
 
+module load snakemake/8.27.0-foss-2024a
 cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/assemblies
+
+snakemake \
+    --snakefile /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/software_install/BRAKER4/Snakefile \
+    --executor slurm \
+    --default-resources slurm_partition=batch mem_mb=120000 \
+    --cores 20 \
+    --jobs 20 \
+    --use-singularity \
+    --singularity-prefix .singularity_cache \
+    --singularity-args "-B /home -B /scratch" \
+    --latency-wait 120 \
+    --restart-times 3
