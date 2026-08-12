b54322e42bb214214a5c689d935bd258c061cd1d
milena-t <milena.trabert@t-online.de>
2026-08-12 12:38:00 +0200

    try with right partition

--- run_braker4_snakemake/run_braker4_snakemake.sh
+++ run_braker4_snakemake/run_braker4_snakemake.sh
@@ -6,9 +6,9 @@ cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/assemblies
 snakemake \
     --snakefile /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/software_install/BRAKER4/Snakefile \
     --executor slurm \
-    --default-resources slurm_partition=batch mem_mb=120000 \
+    --default-resources slurm_partition=core mem_mb=120000 \
     --cores 20 \
-    --jobs 20 \
+    --jobs 50 \
     --use-singularity \
     --singularity-prefix .singularity_cache \
     --singularity-args "-B /home -B /scratch" \
