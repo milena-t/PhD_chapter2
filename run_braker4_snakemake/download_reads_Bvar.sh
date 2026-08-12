ebdc277b9fde47f08ced7fc8feb81f7987080c24
milena-t <milena.trabert@t-online.de>
2026-08-12 14:10:04 +0200

    try to download SRR

---
+++ run_braker4_snakemake/download_reads_Bvar.sh
@@ -0,0 +1,31 @@
+#!/bin/bash -l
+#SBATCH -A uppmax2026-1-8
+#SBATCH -c 8
+#SBATCH -t 1:00:00
+#SBATCH -J download_sequences
+#SBATCH -o download_sequences_Cmac.log
+#SBATCH --mail-type=ALL
+
+module load pigz/2.8-GCCcore-13.3.0
+
+# Define directories
+BASE=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/braker4_annotation/rnaseq_downloads
+SRA=$BASE/B_varius/sra
+FASTQ=$BASE/B_varius/fastq
+SRAPATH=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/software_install/sra_tools/sratoolkit.3.3.0-ubuntu64/bin/
+
+mkdir -p $SRA $FASTQ
+
+
+SRR_IDS=(
+    ERR14379121
+)
+
+for acc in "${SRR_IDS[@]}"; do
+    echo "================== ${acc} =================="
+    ${SRAPATH}prefetch $acc --output-directory $SRA # Download .sra file
+    ${SRAPATH}fasterq-dump --split-files --threads 4 "$acc"
+    gzip "${acc}"*.fastq
+    # rm -rf "$acc"
+done
+
