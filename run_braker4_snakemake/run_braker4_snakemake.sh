01eebe412f36316a561eaf5afae546d85a52a9c8
milena-t <milena.trabert@t-online.de>
2026-08-14 13:27:23 +0200

    update bindmounts

--- run_braker4_snakemake/run_braker4_snakemake.sh
+++ run_braker4_snakemake/run_braker4_snakemake.sh
@@ -18,7 +18,7 @@ cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/braker4_annotati
 # cp -dR --preserve=mode,timestamps --no-preserve=ownership $AUGUSTUS_CONFIG_PATH AUGUSTUS_config
 # chmod -R +w AUGUSTUS_config
 # module unload AUGUSTUS/3.5.0-gfbf-2024a # same as above, some weird shit with conflicting perl versions
-export AUGUSTUS_CONFIG_PATH=$PWD/AUGUSTUS_config
+# export AUGUSTUS_CONFIG_PATH=$PWD/AUGUSTUS_config
 
 snakemake \
     --keep-going \
@@ -29,6 +29,6 @@ snakemake \
     --jobs 50 \
     --use-singularity \
     --singularity-prefix .singularity_cache \
-    --singularity-args "-B /home -B /scratch" \
+    --singularity-args "-B /home -B /scratch -B /gorilla -B /proj" \
     --latency-wait 120 \
     --restart-times 3
