583497b9c5d7eede1f938a2e3e6ddd131a7a5a80
milena-t <milena.trabert@t-online.de>
2026-08-17 10:30:17 +0200

    logname

--- run_braker4_snakemake/run_braker4_snakemake.sh
+++ run_braker4_snakemake/run_braker4_snakemake.sh
@@ -4,8 +4,8 @@
 #SBATCH -c 1
 #SBATCH --mem=10G
 #SBATCH -t 5-00:00:00
-#SBATCH -J braker4_snakemake_Bvar_%j
-#SBATCH -o braker4_snakemake_Bvar_%j.log
+#SBATCH -J braker4_snakemake_Cchi_%j
+#SBATCH -o braker4_snakemake_Cchi_%j.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
