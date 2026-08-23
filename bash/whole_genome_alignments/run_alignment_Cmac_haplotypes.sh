f935df261dfebcceca7dbab0327a25c45466e06d
milena-t <milena.trabert@t-online.de>
2026-08-23 18:10:55 +0200

    cmac wga

--- bash/whole_genome_alignments/run_alignment_Cmac_haplotypes.sh
+++ bash/whole_genome_alignments/run_alignment_Cmac_haplotypes.sh
@@ -1,21 +1,23 @@
 #!/bin/bash -l
 
 
-CMAC_Y_s=/proj/coleoptera-genomics-2025/snic2021-6-30/Philipp/HiFi/Whole_genome_alinment/Alinment_pt_036/pt_036_001_hifiasm_primary.fasta
-CMAC_Y_l=/proj/coleoptera-genomics-2025/snic2021-6-30/Philipp/HiFi/Whole_genome_alinment/Alinment_pt_036/pt_036_002_hifiasm_primary.fasta
+CMAC_Y_s=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/pairwise_wga/Cmac_populations/assemblies/Cmac_china.fasta
+CMAC_Y_l=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/pairwise_wga/Cmac_populations/assemblies/Lome_Yl.fasta
+CMAC_CHINA=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/pairwise_wga/Cmac_populations/assemblies/Lome_Ys.fasta
 
+cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/pairwise_wga/Cmac_populations
 
-for SPECIES1 in $CMAC_Y_s $CMAC_Y_l
+for SPECIES1 in $CMAC_Y_s $CMAC_Y_l $CMAC_CHINA
 do  
 
     SPECIES1_name="${SPECIES1##*/}"
-    SPECIES1_name="${SPECIES1_name%.masked.fna*}"
+    SPECIES1_name="${SPECIES1_name%.fasta*}"
 
-    for SPECIES2 in $CMAC_Y_s $CMAC_Y_l
+    for SPECIES2 in $CMAC_Y_s $CMAC_Y_l $CMAC_CHINA
     do
 
         SPECIES2_name="${SPECIES2##*/}"
-        SPECIES2_name="${SPECIES2_name%.masked.fna*}"
+        SPECIES2_name="${SPECIES2_name%.fasta*}"
 
         if [[ "${SPECIES1_name}" == "${SPECIES2_name}" ]]
         then
