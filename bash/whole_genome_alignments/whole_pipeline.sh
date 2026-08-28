6bbcf51b499dad2b2bade3f89d82474f65b706cf
milena-t <milena.trabert@t-online.de>
2026-08-28 14:52:58 +0200

    back to first setting

--- bash/whole_genome_alignments/whole_pipeline.sh
+++ bash/whole_genome_alignments/whole_pipeline.sh
@@ -48,8 +48,8 @@ echo
 echo " >>>>>>>>>> FILTERING"
 # for different species: -m -i 50 # for different populations of the same species: -r -i 75
 # -r: "Maps each position of each reference to its best hit in the query, allowing for query overlaps", wiht -q being the inverse for the query, and -m being union of -r and -q
-echo "delta-filter -m -i 75 -l 1000 $NUCMER_OUTFILE > ${NUCMER_OUTFILE}_filtered" 
-$MUMMER_PATH/delta-filter -m -i 75 -l 1000 $NUCMER_OUTFILE > ${NUCMER_OUTFILE}_filtered
+echo "delta-filter -r -i 75 -l 1000 $NUCMER_OUTFILE > ${NUCMER_OUTFILE}_filtered" 
+$MUMMER_PATH/delta-filter -r -i 75 -l 1000 $NUCMER_OUTFILE > ${NUCMER_OUTFILE}_filtered
 # echo "$MUMMER_PATH/delta-filter -m $NUCMER_OUTFILE > ${NUCMER_OUTFILE}_filtered"
 # $MUMMER_PATH/delta-filter -m $NUCMER_OUTFILE > ${NUCMER_OUTFILE}_filtered
 
