29ebab76f9f1b045b3802d22a390ffd26c8f3378
milena-t <milena.trabert@t-online.de>
2026-09-11 15:47:30 +0200

    new species

--- bash/MCScanX_scripts/run_MCScanX.sh
+++ bash/MCScanX_scripts/run_MCScanX.sh
@@ -1,19 +1,13 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-148
-#SBATCH -n 1
-#SBATCH -p core
+#SBATCH -A uppmax2026-1-8
+#SBATCH -c 1
 #SBATCH -t 1:00:00
 #SBATCH -J run_MCScanX
 #SBATCH -o run_MCScanX.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
-# Bioinfo tools not needed on pelle
-module load bioinfo-tools 
-
-## test
-# /proj/naiss2023-6-65/Milena/chapter2/MCScanX-1_0_0/MCScanX /proj/naiss2023-6-65/Milena/chapter2/MCScanX_test
-# ./MCScanX test
+# interactive -A uppmax2026-1-8 -t 5:00:00
 
 ## runs really fast for five coleopteran species
 ./MCScanX all_species
