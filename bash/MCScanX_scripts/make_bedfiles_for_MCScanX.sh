93d280eaa8800bcf5a5fda25950c346fcb1d0757
milena-t <milena.trabert@t-online.de>
2026-09-11 12:52:00 +0200

    new annot for mcscanx

--- bash/MCScanX_scripts/make_bedfiles_for_MCScanX.sh
+++ bash/MCScanX_scripts/make_bedfiles_for_MCScanX.sh
@@ -1,22 +1,28 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-148
-#SBATCH -p core
-#SBATCH -n 1
+#SBATCH -A uppmax2026-1-8
+#SBATCH -c 1
 #SBATCH -t 5:00
 #SBATCH -J bedfiles_for_MCScanX
 #SBATCH -o bedfiles_for_MCScanX.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
-module load bioinfo-tools biopython/1.80-py3.10.8
+module load biopython/1.80-py3.10.8
 
-python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/A_obtectus.gff ao
-echo "done A_obtectus.gff"
-python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/B_siliquastri.gff bs
-echo "done B_siliquastri.gff"
-python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/C_chinensis.gff cc
-echo "done C_chinensis.gff"
-python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/C_maculatus_superscaffolded.gff cm
-echo "done C_maculatus_superscaffolded.gff"
-python3 /proj/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/naiss2023-6-65/Milena/chapter2/native_annotations/T_castaneum.gff tc
-echo "done T_castaneum.gff"
+# python3 /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/native_annotations/A_obtectus.gff ao
+# echo "done A_obtectus.gff"
+# python3 /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/native_annotations/B_siliquastri.gff bs
+# echo "done B_siliquastri.gff"
+# python3 /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/native_annotations/C_chinensis.gff cc
+# echo "done C_chinensis.gff"
+# python3 /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/native_annotations/C_maculatus_superscaffolded.gff cm
+# echo "done C_maculatus_superscaffolded.gff"
+# python3 /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/make_bedfile_for_MCScanX.py /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/native_annotations/T_castaneum.gff tc
+# echo "done T_castaneum.gff"
+
+SCIPT_PATH=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/src/
+ANNOT_PATH=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/braker4_annotation/
+python3 ${SCIPT_PATH}make_bedfile_for_MCScanX.py ${ANNOT_PATH}A_obtectus/results/braker_longest.gtf ao
+python3 ${SCIPT_PATH}make_bedfile_for_MCScanX.py ${ANNOT_PATH}B_varius/results/braker_longest.gtf bv
+python3 ${SCIPT_PATH}make_bedfile_for_MCScanX.py ${ANNOT_PATH}C_chinensis/results/braker_longest.gtf cc
+python3 ${SCIPT_PATH}make_bedfile_for_MCScanX.py ${ANNOT_PATH}C_maculatus_china/results/braker_longest.gtf mc
