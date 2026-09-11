ff933c20bc300654ad45cb8cd83338359e625c0b
milena-t <milena.trabert@t-online.de>
2026-09-11 16:25:26 +0200

    plot synteny

---
+++ bash/MCScanX_scripts/plot_MCScanX/plot_ctl_Bvar_Bsil.ctl
@@ -0,0 +1,4 @@
+1000	//dimension (in pixels) of the x-axis
+1500	//dimension (in pixels) of the y-axis
+bs1,bs2,bs10,bs3,bs6,bs5,bs4,bs11,bs7,bs9,bs8	//chromosomes in x-axis
+bv9,bv7,bv13,bv14,bv17,bv3,bv4,bv10,bv6,bv15	//chromosomes in y-axis
--- bash/MCScanX_scripts/plot_MCScanX/plot_mcscanx_pairwise_synteny.sh
+++ bash/MCScanX_scripts/plot_MCScanX/plot_mcscanx_pairwise_synteny.sh
@@ -9,36 +9,47 @@
 
 # Bioinfo tools not needed on pelle
 
-cd /proj/naiss2023-6-65/Milena/chapter2/MCScanX-1_0_0/downstream_analyses
+FILES_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/
+
+cd /proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/MCScanX-1_0_0/downstream_analyses
+
 
 java dual_synteny_plotter \
-    -g /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.gff \
-    -s /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.collinearity \
-    -c /proj/naiss2023-6-65/Milena/chapter2/PhD_chapter2/bash/plot_MCScanX/plot_ctl_Tcas_Bsil.ctl \
-    -o /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/plots/plot_ctl_Tcas_Bsil.png
-
-echo "--> done Tcas Bsil"
-
-java dual_synteny_plotter \
-    -g /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.gff \
-    -s /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.collinearity \
-    -c /proj/naiss2023-6-65/Milena/chapter2/PhD_chapter2/bash/plot_MCScanX/plot_ctl_Cmac_Bsil.ctl \
-    -o /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/plots/plot_ctl_Cmac_Bsil.png
+    -g ${FILES_DIR}MCScanX_results/all_species.gff \
+    -s ${FILES_DIR}MCScanX_results/all_species.collinearity \
+    -c ${FILES_DIR}PhD_chapter2/bash/MCScanX_scripts/plot_MCScanX/plot_ctl_Cmac_Bsil.ctl \
+    -o ${FILES_DIR}MCScanX_results/plots/plot_ctl_Cmac_Bsil.png
 
 echo "--> done Cmac Bsil"
 
 java dual_synteny_plotter \
-    -g /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.gff \
-    -s /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.collinearity \
-    -c /proj/naiss2023-6-65/Milena/chapter2/PhD_chapter2/bash/plot_MCScanX/plot_ctl_Aobt_Bsil.ctl \
-    -o /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/plots/plot_ctl_Aobt_Bsil.png
+    -g ${FILES_DIR}MCScanX_results/all_species.gff \
+    -s ${FILES_DIR}MCScanX_results/all_species.collinearity \
+    -c ${FILES_DIR}PhD_chapter2/bash/MCScanX_scripts/plot_MCScanX/plot_ctl_Aobt_Bsil.ctl \
+    -o ${FILES_DIR}MCScanX_results/plots/plot_ctl_Aobt_Bsil.png
 
 echo "--> done Aobt Bsil"
 
 java dual_synteny_plotter \
-    -g /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.gff \
-    -s /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/all_species.collinearity \
-    -c /proj/naiss2023-6-65/Milena/chapter2/PhD_chapter2/bash/plot_MCScanX/plot_ctl_Cmac_Aobt.ctl \
-    -o /proj/naiss2023-6-65/Milena/chapter2/MCScanX_results/plots/plot_ctl_Cmac_Aobt.png
+    -g ${FILES_DIR}MCScanX_results/all_species.gff \
+    -s ${FILES_DIR}MCScanX_results/all_species.collinearity \
+    -c ${FILES_DIR}PhD_chapter2/bash/MCScanX_scripts/plot_MCScanX/plot_ctl_Dsub_Dcar.ctl \
+    -o ${FILES_DIR}MCScanX_results/plots/plot_ctl_Dsub_Dcar.png
 
-echo "--> done Cmac Aobt"
+echo "--> done Dsub Dcar"
+
+java dual_synteny_plotter \
+    -g ${FILES_DIR}MCScanX_results/all_species.gff \
+    -s ${FILES_DIR}MCScanX_results/all_species.collinearity \
+    -c ${FILES_DIR}PhD_chapter2/bash/MCScanX_scripts/plot_MCScanX/plot_ctl_Bvar_Bsil.ctl \
+    -o ${FILES_DIR}MCScanX_results/plots/plot_ctl_Bvar_Bsil.png
+
+echo "--> done Bvar Bsil"
+
+java dual_synteny_plotter \
+    -g ${FILES_DIR}MCScanX_results/all_species.gff \
+    -s ${FILES_DIR}MCScanX_results/all_species.collinearity \
+    -c ${FILES_DIR}PhD_chapter2/bash/MCScanX_scripts/plot_MCScanX/plot_ctl_Cmac_Bsil.ctl \
+    -o ${FILES_DIR}MCScanX_results/plots/plot_ctl_Cmac_Bsil.png
+
+echo "--> done Cmac Bvar"
