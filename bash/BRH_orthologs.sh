27b0227095afed854a287d9f9ec71e2e5a8cc1b6
milena-t <milena.trabert@t-online.de>
2026-09-20 21:13:38 +0200

    test

--- bash/BRH_orthologs.sh
+++ bash/BRH_orthologs.sh
@@ -12,9 +12,9 @@ ANN_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/native_anno
 SCRIPT_PATH=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/PhD_chapter2/src
 
 
-for SPECIES1 in A_obtectus B_siliquastri B_varius C_chinensis C_maculatus D_carinulata D_sublineata 
+for SPECIES1 in A_obtectus # B_siliquastri B_varius C_chinensis C_maculatus D_carinulata D_sublineata 
 do 
-    for SPECIES2 in A_obtectus B_siliquastri B_varius C_chinensis C_maculatus D_carinulata D_sublineata 
+    for SPECIES2 in B_siliquastri #  B_varius C_chinensis C_maculatus D_carinulata D_sublineata A_obtectus
     do 
 
         [[ "$SPECIES1" == "$SPECIES2" ]] && continue # skip self-blast
