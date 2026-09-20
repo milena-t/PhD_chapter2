f34d01e00157fcc8eb342ce0f3261d4ea97a6beb
milena-t <milena.trabert@t-online.de>
2026-09-20 21:25:56 +0200

    test orthologs

--- src/parse_gff.py
+++ src/parse_gff.py
@@ -339,7 +339,6 @@ def parse_gff3_general(filepath:str, verbose = True, only_genes = False, keep_fe
             if "ID" not in attributes:
                 if gtf:
                     attributes["ID"] = attributes_.strip().split(";")[0].strip()
-                    print(attributes)
                 else:
                     raise RuntimeError(f"no id property found for gene in line: {line}")
             if "Parent" not in attributes and not category==FeatureCategory.Gene and not category==FeatureCategory.Region:
@@ -360,9 +359,9 @@ def parse_gff3_general(filepath:str, verbose = True, only_genes = False, keep_fe
 
             ## add Feature to the output dict
             new_feature=Feature(feature_id=attributes["ID"],contig = contig,category=category,start=int(start),end=int(stop),strandedness=strandedness, frame=frame, parent_id=parent_id)
-            if gtf and category == FeatureCategory.Transcript:
-                print(new_feature)
-                raise RuntimeError("test gtf")
+            # if gtf and category == FeatureCategory.Transcript:
+            #     print(new_feature)
+            #     raise RuntimeError("test gtf")
             genome_annotation[new_feature.feature_id]=new_feature
 
     if verbose:
