6e13011f69f2eef6a83cf981968ae61362851a28
milena-t <milena.trabert@t-online.de>
2026-09-22 14:03:38 +0200

    orthofinder

--- bash/run_orthofinder.sh
+++ bash/run_orthofinder.sh
@@ -1,22 +1,21 @@
 #!/bin/bash -l
-#SBATCH -A uppmax2025-2-148
-#SBATCH -p core
-#SBATCH -n 20
+#SBATCH -A uppmax2026-1-8
+#SBATCH -c 20
 #SBATCH -t 2:00:00
-#SBATCH -J orthofinder_TE_orthoDB
-#SBATCH -o orthofinder_TE_filtered_orthoDB.log
+#SBATCH -J orthofinder_chrysomelidae
+#SBATCH -o orthofinder_chrysomelidae.log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user milena.trabert@ebc.uu.se
 
-module load bioinfo-tools OrthoFinder/2.5.2
+module load OrthoFinder/2.5.5-foss-2024a
 
 # the output directory will be created by orthofinder and should NOT already exist!
-OUT_DIR=/proj/naiss2023-6-65/Milena/chapter2/orthofinder/orthofinder_Cmac_Kaufmann2023
+OUT_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/orthofinder/orthofinder_chrysomelidae
 
 # orthofinder runs with protein sequences here! nucleotide sequences require the -d flag
-IN_FASTA_DIR=/proj/naiss2023-6-65/Milena/chapter2/protein_data/orthofinder_dir
+IN_FASTA_DIR=/proj/coleoptera-genomics-2025/snic2021-6-30/Milena/chapter2/orthofinder/proteins_chrysomelidae
 
 # remove it if it does exist, this overwrites all preexisting results
 rm -r $OUT_DIR
 
-orthofinder.py -t 20 -a 20 -f $IN_FASTA_DIR -o $OUT_DIR
+orthofinder -t 20 -a 20 -f $IN_FASTA_DIR -o $OUT_DIR
